d0e3b2915c85b5fda84c691388afbfc8659027b5
Olle Tidblom <tidblom.ollegmail.com>
2025-04-15 18:21:58 +0200

    Styled the cart page

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('cart/', views.view_cart, name='view_cart'),
     path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/update/<int:item_id>/', views.update_cart, name='update_cart'),
+    path('cart/remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
 from .models import Cart, CartItem
 from products.models import Product
 
@@ -13,4 +14,20 @@ def add_to_cart(request, product_id):
 
 def view_cart(request):
     cart, _ = Cart.objects.get_or_create(user=request.user)
-    return render(request, 'checkout/cart.html', {'cart': cart})
+    quantity_range = range(1, 11)
+    return render(request, 'checkout/cart.html', {'cart': cart, 'quantity_range': quantity_range})
+
+@require_POST
+def update_cart(request, item_id):
+    item = get_object_or_404(CartItem, pk=item_id, cart__user=request.user)
+    quantity = int(request.POST.get('quantity', 1))
+    if quantity > 0:
+        item.quantity = quantity
+        item.save()
+    return redirect('view_cart')
+
+@require_POST
+def remove_from_cart(request, item_id):
+    item = get_object_or_404(CartItem, pk=item_id, cart__user=request.user)
+    item.delete()
+    return redirect('view_cart')
